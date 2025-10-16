0069f0c38820e1a0341e8e9e1dbb959e9fcb1778
Dean Orenstein <dto3576@gmail.com>
2025-10-15 20:55:46 -0500

    script to test webarena (on EC2 instance) (see google drive for our example JSONs)

--- webarena/prepare.sh
+++ webarena/prepare.sh
@@ -3,4 +3,6 @@
 # prepare the evaluation
 # re-validate login information
 mkdir -p ./.auth
-python browser_env/auto_login.py
+
+# Try to run the auto-login script, and if cookies are expired, renew them
+python -m browser_env.auto_login || python -m browser_env.auto_login --site_list all
